1811cf72a28bae4455241e7f5dce0b88a7d43a1c
emrearslan01 <74513320+emrearslan01@users.noreply.github.com>
2024-03-06 23:24:54 +0300

    tamamlandı

---
+++ Models/Project.js
@@ -0,0 +1,68 @@
+const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+
+
+const ProjectSchema = new Schema ({
+    consumption: {
+        type: String,
+        required: true,
+    },
+    consumption_period: {
+        type: String,
+        required: true,
+    },
+    projectscol: {
+        type: String,
+        required: true,
+    },
+    cosine_factor: {
+        type: String,
+        required: true,
+    },
+    export_limit: {
+        type: String,
+        required: true,
+    },
+    notes: {
+        type: String,
+        required: true,
+    },
+    address_id: {
+        type: Schema.Types.ObjectId,
+        ref: "Address",
+    },
+    customer_id: {
+        type: Schema.Types.ObjectId,
+        ref: "Customer",
+    },
+    consumption_profiles_id: {
+        type: Schema.Types.ObjectId,
+        ref: "ConsumptionProfile",
+    }
+
+},
+{timestamps: true}
+);
+
+const Project = mongoose.model("Project", ProjectSchema);
+module.exports = Project;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
