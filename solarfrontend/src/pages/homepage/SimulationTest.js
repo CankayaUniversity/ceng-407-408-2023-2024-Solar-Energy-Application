d34835415bc0670276ab872db28a145538ebc65d
emrearslan01 <74513320+emrearslan01@users.noreply.github.com>
2024-05-09 20:22:38 +0300

    canvas centralization and scrolling problem solved and buttons are collapted out of canvas

--- solarfrontend/src/pages/homepage/SimulationTest.js
+++ solarfrontend/src/pages/homepage/SimulationTest.js
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Experience } from "../../components/Experience";
 import { AddPanel } from "../../components/AddPanel";
-import roofImage from "../../assets/images/roof.jpg";
-import { Vector3 } from "three";
 import * as THREE from "three";
-import { Button, Stack, Box } from "@mui/material";
+import { Button, Grid, Box, Snackbar, Alert } from "@mui/material";
 
 function CameraControlled() {
   const { camera } = useThree();
+
   useEffect(() => {
     const initialDistance = 500;
-    const maxDistance = 1000;
-    const minDistance = 650;
+    const maxDistance = 500;
+    const minDistance = 200;
+
+    camera.position.z = initialDistance;
 
     const updateCameraPosition = () => {
       if (camera.position.z > maxDistance) {
@@ -23,14 +24,18 @@ function CameraControlled() {
       }
     };
 
-    updateCameraPosition();
-
     const handleMouseWheel = (event) => {
-      camera.position.z -= event.deltaY * 0.1;
-      updateCameraPosition();
+      const canvasElement = document.getElementById("simulation-canvas");
+
+      // Check if the mouse is over the canvas
+      if (canvasElement && canvasElement.contains(event.target)) {
+        camera.position.z -= event.deltaY * 0.1;
+        updateCameraPosition();
+        event.preventDefault(); // Prevents scrolling on the page when zooming on the canvas
+      }
     };
 
-    document.addEventListener("wheel", handleMouseWheel);
+    document.addEventListener("wheel", handleMouseWheel, { passive: false });
 
     return () => {
       document.removeEventListener("wheel", handleMouseWheel);
@@ -41,9 +46,6 @@ function CameraControlled() {
 }
 
 function pointInPolygon(point, polygon) {
-  // Bu fonksiyon, verilen bir noktanın (point) verilen bir poligon (polygon) içerisinde olup olmadığını kontrol eder.
-  // Burada basit bir algoritma kullanılmıştır, daha karmaşık geometriler için daha gelişmiş yöntemler gerekebilir.
-
   let isInside = false;
   for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
     if (
@@ -62,117 +64,198 @@ function pointInPolygon(point, polygon) {
 function SimulationTest({ screenshot }) {
   const [isSelecting, setIsSelecting] = useState(false);
   const [showModelPreview, setShowModelPreview] = useState(false);
-  const [addPanelMode, setAddPanelMode] = useState(false); // Güneş paneli ekleme modunu takip etmek için
-  const [panelPosition, setPanelPosition] = useState(new THREE.Vector3()); // Panelin konumunu tutacak
-  const [panels, setPanels] = useState([]); // Yerleştirilen panellerin listesi
+  const [addPanelMode, setAddPanelMode] = useState(false);
+  const [panelPosition, setPanelPosition] = useState(new THREE.Vector3());
+  const [panels, setPanels] = useState([]);
   const [isCancelled, setIsCancelled] = useState(false);
   const [roofSelectionActive, setRoofSelectionActive] = useState(false);
   const [selectedRoofPoints, setSelectedRoofPoints] = useState([]);
   const [selectionStart, setSelectionStart] = useState(null);
   const [selectionEnd, setSelectionEnd] = useState(null);
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [draggingPanelPosition, setDraggingPanelPosition] = useState(null);
 
-  // Güneş paneli ekleme modunu ve önizlemeyi kontrol edecek fonksiyonlar
+  const [roofSelections, setRoofSelections] = useState([]);
+  const interactionActive = isSelecting || roofSelectionActive || addPanelMode;
+
+
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      if (draggingPanelPosition) {
+        setPanelPosition({
+          x: event.clientX,
+          y: event.clientY,
+          z: draggingPanelPosition.z,
+        });
+      }
+    };
+
+    
+    const handleMouseUp = () => {
+      if (draggingPanelPosition && isCancelled) {
+        setPanelPosition(new THREE.Vector3());
+      }
+      setDraggingPanelPosition(null);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [draggingPanelPosition, isCancelled]);
+
+  const handleMouseDown = () => {
+    if (addPanelMode) {
+      setDraggingPanelPosition(panelPosition);
+    }
+  };
+
   const toggleAddPanelMode = () => setAddPanelMode(!addPanelMode);
-
-  const toggleRoofSelection = () =>
-    setRoofSelectionActive(!roofSelectionActive);
-
+  const toggleRoofSelection = () => setRoofSelectionActive(!roofSelectionActive);
   const handleSelectModeToggle = () => {
     setIsSelecting(!isSelecting);
-    setAddPanelMode(false); // Seçim moduna geçildiğinde, panel ekleme modunu kapat
+    setAddPanelMode(false);
   };
 
   const handleAddPanelClick = () => {
     setIsCancelled(false);
     setShowModelPreview(!showModelPreview);
-    setAddPanelMode(!addPanelMode); // Panel ekleme modunu değiştir
+    setAddPanelMode(!addPanelMode);
   };
 
-  // const placePanel = (position) => {
-  //   console.log("panels", panels);
+  const panelWidth = 17;
+  const panelHeight = 25;
 
-  //   if (!isCancelled) {
-  //     setPanels([...panels, position]); // Düzeltme burada yapıldı
-  //     setIsCancelled(false);
-  //   }
-  //   setAddPanelMode(false); // Panel yerleştirildikten sonra modu kapat
-  // };
+  const panelIntersects = (newPanelPosition) => {
+    return panels.some((panel) =>
+      Math.abs(panel.x - newPanelPosition.x) < panelWidth &&
+      Math.abs(panel.y - newPanelPosition.y) < panelHeight
+    );
+  };
 
   const placePanel = (position) => {
+    if (panelIntersects(position)) {
+      setAlertOpen(true);
+      return;
+    }
+
     if (selectionStart != null && selectionEnd != null) {
       let topRight = { x: selectionEnd.x, y: selectionStart.y, z: 0 };
       let bottomLeft = { x: selectionStart.x, y: selectionEnd.y, z: 0 };
       let points = [selectionStart, topRight, selectionEnd, bottomLeft];
       if (pointInPolygon(position, points)) {
-        console.warn("Panel can not placed on obstacles.");
-        return; // Seçilen alanın dışındaysa, işlemi durdur
+        console.warn("Panel cannot be placed on obstacles.");
+        return;
       }
     }
 
     if (!pointInPolygon(position, selectedRoofPoints)) {
       console.warn("Panel can only be placed within the selected area.");
-      return; // Seçilen alanın dışındaysa, işlemi durdur
+      return;
     }
 
     if (!isCancelled) {
-      setPanels([...panels, position]);
+      setPanels((prevPanels) => [...prevPanels, position]);
       setIsCancelled(false);
     }
-    setAddPanelMode(false); // Panel yerleştirildikten sonra modu kapat
+    setAddPanelMode(false);
   };
+  
 
   const handleCancel = () => {
+    setPanels([]);
+    setPanelPosition(new THREE.Vector3());
+    setShowModelPreview(false);
+    setAddPanelMode(false);
+    setIsCancelled(true);
+    handleCancelRoofSelection();
+    setDraggingPanelPosition(null);
+  };
+
+  const handleFinishAddingPanels = () => {
     if (addPanelMode) {
-      setIsCancelled(true); // İptal işlemi gerçekleşti
       setAddPanelMode(false);
-      setShowModelPreview(false);
-      setPanelPosition(new THREE.Vector3()); // Panelin önizleme pozisyonunu sıfırla
+      setIsCancelled(true);
     }
+  };
+
+  const handleCancelRoofSelection = () => {
+    setRoofSelectionActive(false);
+    setSelectedRoofPoints([]);
   };
 
   return (
     <>
-      <Box sx={{  overflowX: 'hidden' ,position: "relative", width: "100vw", height: "100vh" }} >
-        <Stack
+      <Box
+        sx={{
+          overflow: "hidden",
+          position: "relative",
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Grid
+          container
           direction="row"
-          spacing={2}
           sx={{
             position: "absolute",
             zIndex: 100,
             top: "10px",
             left: "10px",
+            width: "fit-content",
           }}
+          spacing={1}
         >
-          <Button variant="contained" onClick={toggleRoofSelection}>
-            {roofSelectionActive ? "Finish Selecting" : "Select Roof Area"}
-          </Button>
-          <Button variant="contained" onClick={handleSelectModeToggle}>
-            {isSelecting ? "Cancel" : "Select Obstacles"}
-          </Button>
-          <Button
-            variant="contained"
-            onClick={handleAddPanelClick}
-            sx={{ alignSelf: "flex-end" }}
-          >
-            Add Solar Panel
-          </Button>
-          <Button
-            variant="contained"
-            onClick={handleCancel}
-            sx={{ alignSelf: "flex-end" }}
-          >
-            Cancel
-          </Button>
-        </Stack>
+          <Grid item>
+            <Button variant="contained" onClick={() => setRoofSelectionActive(!roofSelectionActive)}>
+              {roofSelectionActive ? "Finish Selecting" : "Select Roof Area"}
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={handleCancelRoofSelection}>
+              Cancel Select Roof Area
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={() => setIsSelecting(!isSelecting)}>
+              {isSelecting ? "Cancel" : "Select Obstacles"}
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={() => setAddPanelMode(!addPanelMode)}>
+              {addPanelMode ? "Cancel" : "Add Solar Panels"}
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={() => {
+              setIsCancelled(true);
+              setAddPanelMode(false);
+              setShowModelPreview(false);
+              setPanelPosition(new THREE.Vector3());
+              if (panels.length > 0) {
+                setPanels(panels.slice(0, -1));
+              }
+            }}>
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
 
         <Canvas
+          id="simulation-canvas"
+          ref={canvasRef}
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100vw",
-            height: "100vh",
-            color: "red",
+            width: "55vw",
+            height: "80vh",
+            border: "2px solid #000"
           }}
         >
           <ambientLight intensity={0.5} />
@@ -183,7 +266,22 @@ function SimulationTest({ screenshot }) {
             isSelecting={isSelecting}
             addPanelMode={addPanelMode}
             setPanelPosition={setPanelPosition}
-            onPanelPlace={placePanel}
+            onPanelPlace={(position) => {
+              if (panels.some(panel => {
+                const panelWidth = 17;
+                const panelHeight = 25;
+                return (
+                  Math.abs(panel.x - position.x) < panelWidth &&
+                  Math.abs(panel.y - position.y) < panelHeight
+                );
+              })) {
+                setAlertOpen(true);
+                return;
+              }
+              setPanels(prev => [...prev, position]);
+              setIsCancelled(false);
+              setAddPanelMode(false);
+            }}
             roofSelectionActive={roofSelectionActive}
             setSelectedRoofPoints={setSelectedRoofPoints}
             selectedRoofPoints={selectedRoofPoints}
@@ -195,12 +293,26 @@ function SimulationTest({ screenshot }) {
           {addPanelMode && (
             <AddPanel
               position={panelPosition}
-              isVisible={addPanelMode} // Yeni isVisible prop'u
+              isVisible={addPanelMode}
+              onMouseDown={() => setPanelPosition(panelPosition)}
             />
           )}
-          <OrbitControls enableRotate={false} />
+          <OrbitControls 
+          enableZoom={false}
+          enableRotate={!interactionActive}
+          enablePan={!interactionActive}
+           />
         </Canvas>
       </Box>
+      <Snackbar
+        open={alertOpen}
+        autoHideDuration={6000}
+        onClose={() => setAlertOpen(false)}
+      >
+        <Alert onClose={() => setAlertOpen(false)} severity="error" sx={{ width: "100%" }}>
+          Not available
+        </Alert>
+      </Snackbar>
     </>
   );
 }
