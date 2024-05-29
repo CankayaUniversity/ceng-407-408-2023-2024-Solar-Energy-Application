12a50d0e31772fe97fe6bcc409c805b8416d9344
Bekoavci1 <c1811006@student.cankaya.edu.tr>
2024-05-29 03:05:00 +0300

    Profile alert message update

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -13,12 +13,16 @@ import {
   Snackbar,
   Alert,
   LinearProgress,
-  Box
+  Box,
+  Tabs,
+  Tab,
 } from "@mui/material";
 import CancelIcon from "@mui/icons-material/Cancel";
 import SaveIcon from "@mui/icons-material/Save";
 import { USER } from "../../api/api";
 import { useNavigate } from "react-router-dom";
+import Header from '../dashboard/Header';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function Profile({ onCancel }) {
   const [user, setUser] = useState({
@@ -83,14 +87,19 @@ function Profile({ onCancel }) {
       birthDate: user.birthDate,
     }).then(([data, error]) => {
       if (!error) {
-        setSnackbarMessage("Profil güncellendi!");
+        setSnackbarMessage("Sucess Update Profile!");
         setSnackbarSeverity("success");
         setSnackbarOpen(true);
-        navigate("/paperbase");
+        setTimeout(() => {
+          navigate("/paperbase");
+        }, 2000);
       } else {
-        setSnackbarMessage("Profil güncellenemedi!");
+        setSnackbarMessage("Failed to update user.");
         setSnackbarSeverity("error");
         setSnackbarOpen(true);
+        setTimeout(() => {
+          setSnackbarOpen(false);
+        }, 2000);
       }
     });
   };
@@ -104,121 +113,126 @@ function Profile({ onCancel }) {
   };
 
   return (
-    <Paper style={{ padding: "20px", maxWidth: 500, margin: "20px auto" }}>
-      <Grid container spacing={2} alignItems="center" justifyContent="center">
-        <Grid item xs={12} align="center">
-          <Typography variant="h4">Profile Page</Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Avatar src={user.avatar} sx={{ width: 56, height: 56 }} />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Name"
-            fullWidth
-            name="name"
-            value={user.name}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Email"
-            fullWidth
-            name="email"
-            value={user.email}
-            InputProps={{ readOnly: true }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControl fullWidth>
-            <InputLabel id="gender-select-label">Gender</InputLabel>
-            <Select
-              labelId="gender-select-label"
-              id="gender-select"
-              value={user.gender}
-              label="Gender"
-              onChange={(event) => {
-                setUser((prevUser) => ({
-                  ...prevUser,
-                  gender: event.target.value,
-                }));
-              }}
-            >
-              <MenuItem value="male">Male</MenuItem>
-              <MenuItem value="female">Female</MenuItem>
-              <MenuItem value="other">Other</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Birth Date"
-            fullWidth
-            name="birthDate"
-            type="date"
-            value={user.birthDate}
-            onChange={handleInputChange}
-            InputLabelProps={{ shrink: true }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Current Password or New Password"
-            fullWidth
-            name="password"
-            type="password"
-            value={user.password}
-            onChange={handleInputChange}
-            placeholder="Enter new password"
-          />
-          <Box sx={{ width: '100%', mt: 2, display: 'flex', alignItems: 'center' }}>
-            <LinearProgress
-              variant="determinate"
-              value={passwordStrength}
-              sx={{
-                height: 10,
-                flex: 1,
-                bgcolor: passwordStrength === 100 ? 'green' : 'red'
-              }}
+    <Box sx={{ display: 'flex', minHeight: '100vh', flexDirection: 'column' }}>
+      <CssBaseline />
+      <Header onDrawerToggle={() => {}} onTabChange={() => {}} />
+      <Paper style={{ padding: "20px", maxWidth: 500, margin: "20px auto" }}>
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Grid item xs={12} align="center">
+            <Typography variant="h4">Profile Page</Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Avatar src={user.avatar} sx={{ width: 56, height: 56 }} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Name"
+              fullWidth
+              name="name"
+              value={user.name}
+              onChange={handleInputChange}
             />
-            <Typography variant="body2" sx={{ ml: 2 }}>
-              {`${Math.round(passwordStrength)}%`}
-            </Typography>
-          </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Email"
+              fullWidth
+              name="email"
+              value={user.email}
+              InputProps={{ readOnly: true }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel id="gender-select-label">Gender</InputLabel>
+              <Select
+                labelId="gender-select-label"
+                id="gender-select"
+                value={user.gender}
+                label="Gender"
+                onChange={(event) => {
+                  setUser((prevUser) => ({
+                    ...prevUser,
+                    gender: event.target.value,
+                  }));
+                }}
+              >
+                <MenuItem value="male">Male</MenuItem>
+                <MenuItem value="female">Female</MenuItem>
+                <MenuItem value="other">Other</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Birth Date"
+              fullWidth
+              name="birthDate"
+              type="date"
+              value={user.birthDate}
+              onChange={handleInputChange}
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Current Password or New Password"
+              fullWidth
+              name="password"
+              type="password"
+              value={user.password}
+              onChange={handleInputChange}
+              placeholder="Enter new password"
+            />
+            <Box sx={{ width: '100%', mt: 2, display: 'flex', alignItems: 'center' }}>
+              <LinearProgress
+                variant="determinate"
+                value={passwordStrength}
+                sx={{
+                  height: 10,
+                  flex: 1,
+                  bgcolor: passwordStrength === 100 ? 'green' : 'red'
+                }}
+              />
+              <Typography variant="body2" sx={{ ml: 2 }}>
+                {`${Math.round(passwordStrength)}%`}
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={6} align="right">
+            <Button
+              startIcon={<CancelIcon />}
+              variant="outlined"
+              color="error"
+              onClick={handleCancel}
+            >
+              Cancel
+            </Button>
+          </Grid>
+          <Grid item xs={6} align="left">
+            <Button
+              startIcon={<SaveIcon />}
+              variant="contained"
+              color="primary"
+              onClick={handleSave}
+            >
+              Save
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={6} align="right">
-          <Button
-            startIcon={<CancelIcon />}
-            variant="outlined"
-            color="error"
-            onClick={handleCancel}
-          >
-            Cancel
-          </Button>
-        </Grid>
-        <Grid item xs={6} align="left">
-          <Button
-            startIcon={<SaveIcon />}
-            variant="contained"
-            color="primary"
-            onClick={handleSave}
-          >
-            Save
-          </Button>
-        </Grid>
-      </Grid>
-      <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={6000}
-        onClose={handleSnackbarClose}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      >
-        <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{ width: '100%' }}>
-          {snackbarMessage}
-        </Alert>
-      </Snackbar>
-    </Paper>
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={6000}
+          onClose={handleSnackbarClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          sx={{ '& .MuiSnackbarContent-root': { fontSize: '1.2rem', width: '100%' } }}
+        >
+          <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{ width: '100%' }}>
+            {snackbarMessage}
+          </Alert>
+        </Snackbar>
+      </Paper>
+    </Box>
   );
 }
 
